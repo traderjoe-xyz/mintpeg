cb72088f338803403f0afcfac9575a56eb6e4b0d
analogdev.eth <dejijolaoluwa@gmail.com>
2022-07-11 06:02:32 +0100

    Add test for MintpegFactory setMintpegImplementation function
    In this PR, I added tests for MintpegFactory function to set base implementation of Mintpeg contract

---
+++ test/MintpegFactory.sol/implementation.test.ts
@@ -0,0 +1,55 @@
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { Contract, ContractFactory } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+describe("setMintpegImplementation", () => {
+  let Mintpeg: Contract;
+  let MintpegCF: ContractFactory;
+  let MintpegFactory: Contract;
+  let MintpegFactoryCF: ContractFactory;
+  let dev: SignerWithAddress;
+  let alice: SignerWithAddress;
+
+  beforeEach(async () => {
+    MintpegCF = await ethers.getContractFactory("Mintpeg");
+    Mintpeg = await MintpegCF.deploy();
+    await Mintpeg.deployed();
+
+    MintpegFactoryCF = await ethers.getContractFactory("MintpegFactory");
+    MintpegFactory = await MintpegFactoryCF.deploy();
+    await MintpegFactory.deployed();
+
+    [dev, alice] = await ethers.getSigners();
+  });
+
+  it("should revert when mintpegImplementation is set to a null address", async () => {
+    await expect(
+      MintpegFactory.connect(dev).setMintpegImplementation(
+        ethers.constants.AddressZero
+      )
+    ).to.be.revertedWith("MintpegFactory__InvalidMintpegImplementation");
+  });
+
+  it("should revert when setMintpegImplementation is called by an address other than the owner", async () => {
+    await expect(
+      MintpegFactory.connect(alice).setMintpegImplementation(Mintpeg.address)
+    ).to.be.reverted;
+  });
+
+  it("should set mintpegImplementation to the address passed to function", async () => {
+    await MintpegFactory.connect(dev).setMintpegImplementation(Mintpeg.address);
+
+    expect(await MintpegFactory.getMintpegImplementation()).to.equal(
+      Mintpeg.address
+    );
+  });
+
+  it("should emit `SetMintpegImplementation` when mintpegImplementation is set", async () => {
+    await expect(
+      MintpegFactory.connect(dev).setMintpegImplementation(Mintpeg.address)
+    )
+      .to.emit(MintpegFactory, "SetMintpegImplementation")
+      .withArgs(Mintpeg.address);
+  });
+});
