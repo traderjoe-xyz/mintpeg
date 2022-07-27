af75131318b2d57902832f26bc8bc3eea2d9577f
analogdev.eth <dejijolaoluwa@gmail.com>
2022-07-27 22:17:34 +0100

    Add hardhat task to upload and mint items to a collection
    In this PR, I added a hardhat task --mint-collection-items-- which contains logic to upload and mint new items to a collection using IPFS CIDs

---
+++ tasks/mint-collection-items.ts
@@ -0,0 +1,69 @@
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+import axios from "axios";
+import FormData from "form-data";
+import fs, { ReadStream } from "fs";
+import { task } from "hardhat/config";
+import { loadMintConfig } from "./utils";
+
+interface AddCollectionItemsProps {
+  metaFilename: string;
+  itemFilename: string;
+  mintpegAddress: string;
+}
+
+task("mint-collection-items", "Mint new NFTs to a Mintpeg contract instance")
+  .addParam("metaFilename")
+  .addParam("itemFilename")
+  .addParam("mintpegAddress")
+  .setAction(
+    async (
+      { metaFilename, itemFilename, mintpegAddress }: AddCollectionItemsProps,
+      hre
+    ) => {
+      console.log("-- Reading Images and Metadatas --");
+
+      // create form-data
+      const form = new FormData();
+      const metaDatas: Array<Object> = loadMintConfig(metaFilename);
+      const imageNames: string[] = loadMintConfig(itemFilename);
+      if (metaDatas.length !== imageNames.length) {
+        throw new Error(
+          `Mismatch config count: ${metaDatas.length} metadata gotten for ${imageNames.length} images`
+        );
+      }
+
+      imageNames.forEach((image) => {
+        const pathToImage = __dirname + "/config/mint/images/" + image;
+        if (!fs.existsSync(pathToImage)) {
+          throw new Error(`/mint/images/${image} not found.`);
+        }
+        form.append("images", fs.createReadStream(pathToImage));
+      });
+      form.append("metadatas", JSON.stringify(metaDatas));
+
+      try {
+        console.log("-- Uploading Images to IPFS --");
+
+        const ipfsCidUrls: string[] = await axios.post(
+          "http://localhost:8000/v2/mints/upload",
+          form,
+          { headers: form.getHeaders() }
+        );
+
+        console.log(`-- Minting Token(s) from IPFS CID(s) --`);
+
+        const ethers = hre.ethers;
+        const mintpegContract = await ethers.getContractAt(
+          "Mintpeg",
+          mintpegAddress
+        );
+        const mintTx = await mintpegContract.mint(ipfsCidUrls);
+        await mintTx.wait();
+        console.log(`-- Minted ${ipfsCidUrls.length} tokens ${mintpegAddress}  --`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
--- tasks/utils.ts
+++ tasks/utils.ts
@@ -7,6 +7,12 @@ export const loadLaunchConfig = (filename: string) => {
   return launchConfig;
 };
 
+export const loadMintConfig = (filename: string) => {
+  const file = path.join(__dirname, `config/mint/${filename}`);
+  const mintConfig = JSON.parse(fs.readFileSync(file, "utf8"));
+  return mintConfig;
+};
+
 export const delay = async (seconds: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 };
