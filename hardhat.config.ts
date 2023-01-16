0ae57c7710dc35a4221a2d5e6626f9026b6a7c9f
pecio222 <pecio222@gmail.com>
2023-01-17 00:19:08 +0100

    add bsc config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ module.exports = {
     hardhat: {},
     fuji: {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
-      gasPrice: 26000000000,
+      gasPrice: 26_000_000_000,
       chainId: 43113,
       accounts: process.env.DEPLOY_PRIVATE_KEY
         ? [`0x${process.env.DEPLOY_PRIVATE_KEY}`]
@@ -23,10 +23,26 @@ module.exports = {
     },
     avalanche: {
       url: "https://api.avax.network/ext/bc/C/rpc",
-      gasPrice: 26000000000,
+      gasPrice: 26_000_000_000,
       chainId: 43114,
       accounts: process.env.DEPLOY_PRIVATE_KEY
         ? [`0x${process.env.DEPLOY_PRIVATE_KEY}`]
+        : [],
+    },
+    bscTestnet: {
+      url: process.env.BNB_RPC_ENDPOINT,
+      gasPrice: 20_000_000_000,
+      chainId: 97,
+      accounts: process.env.BNB_TESTNET_DEPLOYER
+        ? [process.env.BNB_TESTNET_DEPLOYER]
+        : [],
+    },
+    bsc: {
+      url: process.env.BSC_RPC_ENDPOINT ? process.env.BSC_RPC_ENDPOINT : "",
+      gasPrice: 5_000_000_000,
+      chainId: 56,
+      accounts: process.env.BSC_TESTNET_DEPLOYER
+        ? [process.env.BSC_TESTNET_DEPLOYER]
         : [],
     },
   },
@@ -37,6 +53,11 @@ module.exports = {
     deployer: 0,
   },
   etherscan: {
-    apiKey: process.env.SNOWTRACE_API_KEY ?? "",
+    apiKey: {
+      avalanche: process.env.SNOWTRACE_API_KEY,
+      avalancheFujiTestnet: process.env.SNOWTRACE_API_KEY,
+      bscTestnet: process.env.BNB_API_KEY,
+      bsc: process.env.BSC_API_KEY,
+    },
   },
 };
